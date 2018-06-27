c168863411400c1c7ee9eaf30b4af2d04cd26616
Michael Smith <michaelsmith19951@yahoo.com>
2018-06-27 19:37:59 -0400

    2nd test that failed, first_item_in_array_is_blue

--- tdd_methods.rb
+++ tdd_methods.rb
@@ -1,4 +1,28 @@
-# First function for array.count method
-	def array_count(go_argument)
-	array = []
-	end
+# First function for array.first method
+	def array_first(my_arr)
+		counter = 0
+		my_arr.each do |item|
+			counter = counter + 1
+		end
+		
+
+end
+
+# Second function for hash.length method
+
+	# def count_1_to_100(go_argument_hash)
+	# 	count = [1..100]
+	# 	count
+	# end
+
+# Third function for string.clear method
+	
+
+# Count elements in array
+	# def count_elements_in_array(my_arr)
+	# 	counter = 0
+	# 	my_arr.each do |item|
+	# 		counter = counter + 1
+	# 	end
+	# 	counter
+	# end
--- test_tdd_methods.rb
+++ test_tdd_methods.rb
@@ -8,8 +8,21 @@ require_relative "tdd_methods.rb"
 class TestTddMethods < Minitest::Test
 
 # Functions for each test
-	def test_if_array_contains_number_5
-	assert_equal(5, array_count("go_argument"))
+	def test_if_first_item_in_array_is_blue
+	my_arr = ["blue", "red", "yellow", "green", "orange"]
+	assert_equal(5, array_first(my_arr))
 	end
 
+	# def test_if_hash_counts_to_100
+	# 	assert_equal(count, count_1_to_100("go_argument_hash"))		
+	# end
+
+	# def test_if_array_has_5_items
+	# 	my_arr = ["l", "g", 5, " ", true]
+	# 	my_other_arr = [*1..100]
+	# 	my_next_arr = ["a", "b", "c", "d", "e", "f", "g"]
+	# 	assert_equal(5, count_elements_in_array(my_arr))
+	
 end
+	
+	
